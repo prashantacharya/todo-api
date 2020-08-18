41eb581af3355dd3424e36e82a534831c37fa4f6
Prashant Acharya <prashant.acharya@sagarmatha.edu.np>
2020-08-18 13:29:31 +0545

    Add CORS

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
+import cors from 'cors';
 import express from 'express';
 import { config } from 'dotenv';
+
 import apiRouter from './api.routes';
 import createError from './utils/createError';
 config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', apiRouter);
 
